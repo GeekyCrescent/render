c5e9dcac5fc20382849b19ac2b5650fe18c3d014
Juan Rodrigo Ferro Rayón <101296886+GeekyCrescent@users.noreply.github.com>
2025-03-11 12:35:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,58 @@
-from typing import Optional
+from fastapi import FastAPI, Form
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import Annotated
 
-from fastapi import FastAPI
+class Item(BaseModel):
+    pnumero: str
+    snumero: str
+
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/suma")
+def suma(numero: Annotated[Item, Form()]):
+    x=float(numero.pnumero)
+    y=float(numero.snumero)
+    resultado = x + y
+    return str(resultado)
+
+@app.post("/resta")
+def resta(numero: Annotated[Item, Form()]):
+    x = float(numero.pnumero)
+    y = float(numero.snumero)
+    resultado = x - y
+    return str(resultado)
+
+@app.post("/multiplicacion")
+def multiplicacion(numero: Annotated[Item, Form()]):
+    x = float(numero.pnumero)
+    y = float(numero.snumero)
+    resultado = x * y
+    return str(resultado)
+
+@app.post("/division")
+def division(numero: Annotated[Item, Form()]):
+    x = float(numero.pnumero)
+    y = float(numero.snumero)
+    
+    if y == 0:
+        return {"error": "No se puede dividir entre 0"}
+    
+    resultado = x / y
+    return str(resultado)
